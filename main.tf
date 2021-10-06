a0e600e0adedfe67e08198c9244acf1201243048
stazera <bighornsystems@gmail.com>
2021-10-06 16:06:21 -0500

    multiple files commit

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-    cidr_block            = "10.0.0.0/24"
+    cidr_block            = var.vpc_cidr
     enable_dns_support    = true
     enable_dns_hostnames  = true
 
     tags = {
-      "Name" = "labs-1"
+      "Name" = "labs-demo"
     } 
 }
