32244fb2ef7dc454fbd68ae1c7fdba5c97382380
stazera <bighornsystems@gmail.com>
2021-10-06 13:57:04 -0500

    add vpc

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ provider "aws" {
     region = var.region
 }
 
+resource "aws_vpc" "main" {
+    cidr_block            = var.vpc_cidr
+    enable_dns_support    = true
+    enable_dns_hostnames  = true
+
+    tags = {
+      "Name" = "labs-1"
+    } 
+}
